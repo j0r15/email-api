0b9f976f066e9d2265e2b65f96b2e9f07bb2335a
j0r15 <joris@baldr.eu>
2024-09-28 19:58:07 +0200

    add require tls

--- src/config/mailer.config.js
+++ src/config/mailer.config.js
@@ -6,6 +6,7 @@ const transport = nodemailer.createTransport({
   host: process.env.EMAIL_SERVER,
   port: process.env.EMAIL_PORT,
   from: process.env.EMAIL_FROM,
+  requireTLS: process.env.REQUIRE_TLS,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
